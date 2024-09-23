fc2f79a9252c43075d03f6f59d86796a89ca1328
Owen Burnett <oburnett127@gmail.com>
2024-09-22 20:02:00 -0500

    added eas.json

--- src/pages/LogoutPage.tsx
+++ src/pages/LogoutPage.tsx
@@ -1,13 +1,14 @@
 import React, { useContext } from 'react';
 import { UserContext } from '../components/UserContext';
 import { Text } from 'react-native';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 function LogoutPage() {
   const userContext = useContext(UserContext);
   if (!userContext) return null;
   const { setIsLoggedIn } = userContext;
    
-  localStorage.removeItem('jwtToken');
+  AsyncStorage.removeItem('jwtToken');
   setIsLoggedIn(false); 
   
   return (
